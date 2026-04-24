1bcfa362d7477f62b2cc30d3c8f9e1339b9e6688
huynhtienanh <129275735+huynhtienanh@users.noreply.github.com>
2026-04-24 23:00:42 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,60 @@
 #include <stdio.h>
 
 int main(void) {
+    char name[50];
+    char unisex[50];
+    char dateofbirth[50];
+    char maritalstatus[50];
+    char hometown[50];
+    float height;
+    int age;
+    float weight;
+    unsigned int netassets;
 
-    float mathscore = 8;
-    float physicspoints = 7;
-    float Literaturescore = 5;
-    int yearofbirth = 1999;
-    unsigned long long int budget = 8000000000;
-    float Vntousd = (float) budget / 26453.5f;
-    float averagescore = (mathscore + physicspoints + Literaturescore) / 3;
+// họ và tên
+    printf("ho va ten: ");
+    scanf("%s", &name);
+
+// giới tính
+    printf("gioi tinh: ");
+    scanf("%s", &unisex);
+
+//tuổi tác
+    printf("tuoi tac: ");
+    scanf("%d", &age);
+
+//ngày thánh năm sinh
+    printf("ngay thang nam sinh: ");
+    scanf("%s", &dateofbirth);
+
+// tình trạng hôn nhân
+    printf("tinh trang hon nhan: ");
+    scanf("%s", &maritalstatus);
+
+//quê quán
+    printf("que quan: ");
+    scanf("%s", &hometown);
+
+//chiều cao
+    printf("chieu cao: ");
+    scanf("%f", &height);
+
+// cân nặng
+    printf("can nang:");
+    scanf("%f", &weight);
+
+//tại sản ròng
+    printf("tai san rong: ");
+    scanf("%u", &netassets);
 
 
-      printf("diem toan: %.1f\n", mathscore);
-      printf("diem ly: %.1f\n", physicspoints);
-      printf("diem van: %.1f\n", Literaturescore);
-      printf("nam sinh: %d\n", yearofbirth);
-      printf("ngan sach: %llu\n", budget);
-      printf("diem trung binh: %.1f\n", averagescore);
-      printf("chuyen doi vn sang usd: %.1f\n", Vntousd);
+    printf("%s\n", name);
+    printf("%s\n", unisex);
+    printf("%s\n", dateofbirth);
+    printf("%s\n", maritalstatus);
+    printf("%.2f\n", height);
+    printf("%.1f\n", weight);
+    printf("%u\n", netassets);
 
 
     return 0;
