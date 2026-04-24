63a0599f31e4144c5f546256519cbbc5e8bccd72
huynhtienanh <129275735+huynhtienanh@users.noreply.github.com>
2026-04-24 23:00:00 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
 int main(void) {
-    int a;
-    printf("Enter number: ");
-    scanf("%d", &a);
-    if (a==0) {
-        printf("neither even nor odd ");
-        scanf ("%d",&a);
-    } if ( a%2 == 0 ) {
-        printf(" old number ");
-    }    else {
-        printf ("even number ");
 
-    }
+    float mathscore = 8;
+    float physicspoints = 7;
+    float Literaturescore = 5;
+    int yearofbirth = 1999;
+    unsigned long long int budget = 8000000000;
+    float Vntousd = (float) budget / 26453.5f;
+    float averagescore = (mathscore + physicspoints + Literaturescore) / 3;
+
+
+      printf("diem toan: %.1f\n", mathscore);
+      printf("diem ly: %.1f\n", physicspoints);
+      printf("diem van: %.1f\n", Literaturescore);
+      printf("nam sinh: %d\n", yearofbirth);
+      printf("ngan sach: %llu\n", budget);
+      printf("diem trung binh: %.1f\n", averagescore);
+      printf("chuyen doi vn sang usd: %.1f\n", Vntousd);
+
+
     return 0;
-
 }
